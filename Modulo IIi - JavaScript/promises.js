0eaef861d67baae0e0db2fddf126ba981a71a022
Paulo Roberto <evans_sp@hotmail.com>
2022-04-11 16:58:46 -0300

    add promises

---
+++ Modulo IIi - JavaScript/promises.js
@@ -0,0 +1,18 @@
+async function resolvePromise()
+{
+    const myPromise = new Promise((resolve, reject) => 
+    {
+        window.setTimeout(() => 
+        {
+            resolve('Resolvida');
+        }, 3000);
+});
+
+const resolved = await myPromise
+.then((result ) => result + 'Passando pelo then')
+.then((result ) => result + ' e agora acabou')
+.catch((err)=> console.log(err.message));
+
+return resolved;
+
+}

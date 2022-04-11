e4fb43747e5f741ea01f828eac3243b33887f222
Paulo Roberto <evans_sp@hotmail.com>
2022-04-11 17:33:03 -0300

    add params conta bancaria

--- Modulo IIi - JavaScript/contaBancaria/scripts.js
+++ Modulo IIi - JavaScript/contaBancaria/scripts.js
@@ -2,7 +2,10 @@ class ContaBancaria {
     //construindo o construtor da classe
 
     constructor(agencia, numero, tipo, saldo){
-        
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this.saldo = saldo;
     }
 
 }

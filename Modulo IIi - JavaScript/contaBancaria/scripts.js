d1c9547ec70cc9010a5f13a7bedb9dc8af874d46
Paulo Roberto <evans_sp@hotmail.com>
2022-04-11 17:41:32 -0300

    add classe completa

--- Modulo IIi - JavaScript/contaBancaria/scripts.js
+++ Modulo IIi - JavaScript/contaBancaria/scripts.js
@@ -1,45 +1,72 @@
 class ContaBancaria {
-    //construindo o construtor da classe
+	constructor(agencia, numero, tipo) {
+		this.agencia = agencia;
+		this.numero = numero;
+		this.tipo = tipo;
+		this._saldo = 0;
+	}
 
-    constructor(agencia, numero, tipo, saldo){
-        this.agencia = agencia;
-        this.numero = numero;
-        this.tipo = tipo;
-        //incluindo o _ na frente para mostrar que ele terá um getter e setter
-        this._saldo = saldo;
-    }
+	sacar(valor) {
+		if (valor > this._saldo) {
+			return console.log('Saque negado; saldo insuficiente!');
+		}
 
-    //add getter e setter
-    get saldo(){
-        return this._saldo;
-    }
+		this._saldo = this._saldo - valor;
+		return this._saldo;
+	}
 
-    set saldo(valor){
-        this._saldo = valor;
-    }
+	depositar(valor) {
+		this._saldo = this._saldo + valor;
+		return this._saldo;
+	}
 
+	set saldo(valor) {
+		this._saldo = valor;
+	}
 
-    sacar(valor) {
-        if(valor > this._saldo){
-            return "Operação Negada"
-        }
-        this._saldo = this._saldo - valor;
-        
-        //Retorna o saldo
-        return this._saldo;
-    }
-    depositar(valor) {
-        this._saldo = this._saldo + valor;
-
-        return this._saldo;
-    }
-
+	get saldo() {
+		return this._saldo;
+	}
 }
 
-class ContaCorrente {
-constructor (agencia, numero, saldo, cartaoCredito){
-    super(agencia, numero, saldo);
-    this.tipo = 'corrente';
-    this._cartaoCresito = cartaoCredito;
+class ContaCorrente extends ContaBancaria {
+	constructor(agencia, numero, cartaoCredito) {
+		super(agencia, numero);
+		this.tipo = 'corrente';
+		this._cartaoCredito = cartaoCredito;
+	}
+
+	set cartaoCredito(valor) {
+		this._cartaoCredito = valor;
+	}
+
+	get cartaoCredito() {
+		return this._cartaoCredito;
+	}
 }
+
+class ContaPoupanca extends ContaBancaria {
+	constructor(agencia, numero) {
+		super(agencia, numero);
+		this.tipo = 'poupança';
+	}
 }
+
+class ContaUniversitaria extends ContaBancaria {
+	constructor(agencia, numero) {
+		super(agencia, numero);
+		this.tipo = 'universitária';
+	}
+
+	sacar(valor) {
+		if (valor > 500) {
+			return 'Operação negada.';
+		}
+
+		this._saldo = this._saldo - valor;
+		return this._saldo;
+	}
+}
+
+const minhaConta = new ContaCorrente(1, 211, true);
+const contaUni = new ContaUniversitaria(2, 333);
